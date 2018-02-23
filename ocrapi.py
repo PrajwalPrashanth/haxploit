1d313a1ecee02475803fb28e03fa36add725073e
Abhishek Dharani <33752861+Dharani-abss@users.noreply.github.com>
2018-02-23 09:54:49 -0800

    Update ocrapi.py

--- ocrapi.py
+++ ocrapi.py
@@ -1,69 +1,57 @@
 import requests
 
 
-class OCRSpaceLanguage:
-    Arabic = 'ara'
-    Bulgarian = 'bul'
-    Chinese_Simplified = 'chs'
-    Chinese_Traditional = 'cht'
-    Croatian = 'hrv'
-    Danish = 'dan'
-    Dutch = 'dut'
-    English = 'eng'
-    Finnish = 'fin'
-    French = 'fre'
-    German = 'ger'
-    Greek = 'gre'
-    Hungarian = 'hun'
-    Korean = 'kor'
-    Italian = 'ita'
-    Japanese = 'jpn'
-    Norwegian = 'nor'
-    Polish = 'pol'
-    Portuguese = 'por'
-    Russian = 'rus'
-    Slovenian = 'slv'
-    Spanish = 'spa'
-    Swedish = 'swe'
-    Turkish = 'tur'
+def ocr_space_file(filename, overlay=False, api_key='a311ed074988957', language='eng'):
+    """ OCR.space API request with local file.
+        Python3.5 - not tested on 2.7
+    :param filename: Your file path & name.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+
+    payload = {'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    with open(filename, 'rb') as f:
+        r = requests.post('https://api.ocr.space/parse/image',
+                          files={filename: f},
+                          data=payload,
+                          )
+    return r.content.decode()
 
 
-class OCRSpace:
-    def __init__(self, api_key, language=OCRSpaceLanguage.English):
-        """ ocr.space API wrapper
-        :param api_key: API key string
-        :param language: document language
-        """
-        self.api_key = 'a311ed074988957'
-        self.language = 'eng'
-        self.payload = {
-            'isOverlayRequired': True,
-            'apikey': self.api_key,
-            'language': self.language,
-        }
+def ocr_space_url(url, overlay=False, api_key='helloworld', language='eng'):
+    """ OCR.space API request with remote file.
+        Python3.5 - not tested on 2.7
+    :param url: Image url.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
 
-    def ocr_file(self, filename):
-        """ OCR.space API request with local file
-        :param filename: Your file path & name
-        :return: Result in JSON format
-        """
-        with open(filename, 'rb') as f:
-            r = requests.post(
-                'https://api.ocr.space/parse/image',
-                files={filename: f},
-                data=self.payload,
-            )
-        return r.json()
+    payload = {'url': url,
+               'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    r = requests.post('https://api.ocr.space/parse/image',
+                      data=payload,
+                      )
+    return r.content.decode()
 
-    def ocr_url(self, url):
-        """ OCR.space API request with remote file
-        :param url: Image url
-        :return: Result in JSON format.
-        """
-        data = self.payload
-        data['url'] = url
-        r = requests.post(
-            'https://api.ocr.space/parse/image',
-            data=data,
-        )
-return r.json()
+
+# Use examples:
+test_file = ocr_space_file(filename='example_image.png', language='pol')
+test_url = ocr_space_url(url='http://i.imgur.com/31d5L5y.jpg')
